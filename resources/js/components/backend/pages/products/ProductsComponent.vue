ccacc822c8a057d897efddbad641153f7c9811dc
jo316-dev <jo316.dev@gmail.com>
2021-08-11 07:02:32 -0300

    load produtos

--- resources/js/components/backend/pages/products/ProductsComponent.vue
+++ resources/js/components/backend/pages/products/ProductsComponent.vue
@@ -1,6 +1,34 @@
 <template>
     <div>
 
+        <div class="row">
+            <div class="col">
+                <h3>Produtos</h3>
+            </div>
+        </div>
+
+         <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Item</th>
+                    <th>Descrição</th>
+                    <th>Valor</th>
+                </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(product, index) in products.data" :key="index">
+
+                  <td>{{ product.id}}</td>
+                  <td>{{ product.name}}</td>
+                  <td>{{ product.description}}</td>
+                  <td>{{ product.price}}</td>
+
+              </tr>
+
+            </tbody>
+        </table>
+
     </div>
 </template>
 
@@ -12,13 +40,22 @@ export default {
        this.loadProducts()
    },
 
+   computed:{
+
+       products () {
+           return this.$store.state.products.items
+       }
+
+   },
+
    methods:{
        loadProducts () {
 
             this.$store.dispatch('loadProducts')
 
        }
-   }
+   },
+  
  
     
 }
